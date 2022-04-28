27e31687b03763c296760149943961669f7b051e
Christoph Hahn <chrhhn@gmail.com>
2022-04-28 16:12:54 +0200

    Implemente basic movement of Level Sprites...

--- src/Level.cpp
+++ src/Level.cpp
@@ -9,5 +9,12 @@ void game::Level::Draw() {
 }
 
 void game::Level::Update() {
-
+    // Achtung: Einziger Haken hier ist, wir durchlaufen den Vector 2x
+    // einmal beim Update(), einmal beim Draw()
+    // Kann man optimieren. Aber Vektoren zu durchlaufen ist etwas, das man
+    // dauernd macht und nie ganz optimieren kann, zumindest nicht, ohne
+    // das die Lesbarkeit des Codes deutlich leidet...
+    for (const auto &sprite: sprites) {
+        sprite->pos_x += 5;
+    }
 }
